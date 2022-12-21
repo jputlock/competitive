c3468cf7ca7c6ab5a926124e3d7913227868a0de
Jeff Putlock <jeffreyputlock62@gmail.com>
2022-12-21 01:28:23 -0500

    dayz

--- adventofcode/solvers/year2022/day19/solver.py
+++ adventofcode/solvers/year2022/day19/solver.py
@@ -1,14 +1,182 @@
 from typing import Generator
 from collections import defaultdict as dd
+from collections import deque
 import numpy as np
 import networkx as nx
 import re, math, itertools
 
-offsets = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+def simulate_blueprint(
+    ore_cost: int,
+    clay_ore_cost: int,
+    obs_ore_cost: int,
+    obs_clay_cost: int,
+    geo_ore_cost: int,
+    geo_obs_cost: int,
+    total_time: int,
+):
+    num_geodes = 0
+
+    MAX_ORE = max(ore_cost, clay_ore_cost, obs_ore_cost, geo_ore_cost)
+
+    starting_state = (1, 0, 0, 0, 0, 0, 0, 0, total_time)
+    queue = deque([starting_state])
+    all_states = set()
+
+    while queue:
+        (
+            ore_bots,
+            clay_bots,
+            obs_bots,
+            geo_bots,
+            ore,
+            clay,
+            obsidian,
+            geodes,
+            time_left,
+        ) = queue.popleft()
+
+        assert (
+            ore_bots <= MAX_ORE
+            and clay_bots <= obs_clay_cost
+            and obs_bots <= geo_obs_cost
+        )
+
+        num_geodes = max(num_geodes, geodes)
+        if time_left == 0:
+            continue
+
+        ore = min(ore, MAX_ORE * time_left - ore_bots * (time_left - 1))
+        clay = min(clay, obs_clay_cost * time_left - clay_bots * (time_left - 1))
+        obsidian = min(obsidian, geo_obs_cost * time_left - obs_bots * (time_left - 1))
+
+        state = (
+            ore_bots,
+            clay_bots,
+            obs_bots,
+            geo_bots,
+            ore,
+            clay,
+            obsidian,
+            geodes,
+            time_left,
+        )
+
+        if state in all_states:
+            # We already know this subproblem
+            continue
+
+        # Otherwise, we do the subproblem
+        all_states.add(state)
+
+        # Option 1: do nothing
+        queue.append(
+            (
+                ore_bots,
+                clay_bots,
+                obs_bots,
+                geo_bots,
+                ore + ore_bots,
+                clay + clay_bots,
+                obsidian + obs_bots,
+                geodes + geo_bots,
+                time_left - 1,
+            )
+        )
+
+        # Option 2: build ore bot
+        if ore_bots < MAX_ORE and ore >= ore_cost:
+            queue.append(
+                (
+                    ore_bots + 1,
+                    clay_bots,
+                    obs_bots,
+                    geo_bots,
+                    ore - ore_cost + ore_bots,
+                    clay + clay_bots,
+                    obsidian + obs_bots,
+                    geodes + geo_bots,
+                    time_left - 1,
+                )
+            )
+
+        # Option 3: build clay bot
+        if clay_bots < obs_clay_cost and ore >= clay_ore_cost:
+            queue.append(
+                (
+                    ore_bots,
+                    clay_bots + 1,
+                    obs_bots,
+                    geo_bots,
+                    ore - clay_ore_cost + ore_bots,
+                    clay + clay_bots,
+                    obsidian + obs_bots,
+                    geodes + geo_bots,
+                    time_left - 1,
+                )
+            )
+
+        # Option 4: build obsidian bot
+        if obs_bots < geo_obs_cost and ore >= obs_ore_cost and clay >= obs_clay_cost:
+            queue.append(
+                (
+                    ore_bots,
+                    clay_bots,
+                    obs_bots + 1,
+                    geo_bots,
+                    ore - obs_ore_cost + ore_bots,
+                    clay - obs_clay_cost + clay_bots,
+                    obsidian + obs_bots,
+                    geodes + geo_bots,
+                    time_left - 1,
+                )
+            )
+
+        # Option 5: build geode bot
+        if ore >= geo_ore_cost and obsidian >= geo_obs_cost:
+            queue.append(
+                (
+                    ore_bots,
+                    clay_bots,
+                    obs_bots,
+                    geo_bots + 1,
+                    ore - geo_ore_cost + ore_bots,
+                    clay + clay_bots,
+                    obsidian - geo_obs_cost + obs_bots,
+                    geodes + geo_bots,
+                    time_left - 1,
+                )
+            )
+    return num_geodes
 
 
-def solve(input: str) -> Generator[any, None, None]:
-    lines = [line.strip() for line in input.split("\n") if line]
+def solve(inp: str) -> Generator[any, None, None]:
+
+    blueprints = []
+    for line in inp.split("\n"):
+        parts = line.split(".")
+        ore = int(re.search(r"(\d+) ore", parts[0]).group(1))
+        clay = int(re.search(r"(\d+) ore", parts[1]).group(1))
+        match = re.search(r"(\d+) ore and (\d+) clay", parts[2])
+        obsidian = (int(match.group(1)), int(match.group(2)))
+        match = re.search(r"(\d+) ore and (\d+) obsidian", parts[3])
+        geode = (int(match.group(1)), int(match.group(2)))
+
+        blueprints.append((ore, clay, *obsidian, *geode))
+
+    quality = 0
+    for i, bp in enumerate(blueprints, 1):
+        num_geodes = simulate_blueprint(*bp, 24)
+        print(f"{i}:\t{num_geodes}")
+        quality += i * num_geodes
 
     # Part 1
-    yield 0
+    yield int(quality)
+
+    # Part 2
+    answer = 1
+    for i, bp in enumerate(blueprints[:3], 1):
+        num_geodes = simulate_blueprint(*bp, 32)
+        print(f"{i}:\t{num_geodes}")
+        answer *= num_geodes
+    yield answer
--- adventofcode/solvers/year2022/day20/solver.py
+++ adventofcode/solvers/year2022/day20/solver.py
@@ -1,4 +1,4 @@
-from typing import Generator
+from typing import Generator, Optional
 from collections import defaultdict as dd
 import numpy as np
 import networkx as nx
@@ -7,8 +7,99 @@ import re, math, itertools
 offsets = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
 
+class Node:
+    def __init__(self, val):
+        self.value: int = val
+        self.prev: Node = self
+        self.next: Node = self
+
+
+class LinkedList:
+    def __init__(self, vals: list, index_map: dict):
+        self.head: Optional[Node] = None
+        prev: Optional[Node] = None
+        current: Optional[Node] = None
+
+        for i, val in enumerate(vals):
+            current = Node(val)
+            current.prev = prev
+
+            if self.head is None:
+                self.head = current
+            if prev is not None:
+                prev.next = current
+
+            index_map[i] = current
+
+            prev = current
+        # Make it circular
+        self.head.prev = current
+        current.next = self.head
+        self.len = len(vals)
+
+    def move(self, node: Node, amount: int):
+        # Remove this node from the list
+        node.prev.next = node.next
+        node.next.prev = node.prev
+
+        # Calculate the displacement in a list without this element
+        amount = amount % (self.len - 1)
+
+        # Calculate the node before where this should be re-inserted
+        curr = node.prev
+        for _ in range(amount):
+            curr = curr.next
+
+        # Fix this node's pointers
+        node.prev = curr
+        node.next = curr.next
+
+        # Fix the surrounding node pointers
+        curr.next.prev = node
+        curr.next = node
+
+
 def solve(input: str) -> Generator[any, None, None]:
-    lines = [line.strip() for line in input.split("\n") if line]
 
     # Part 1
-    yield 0
+    vals = [int(line.strip()) for line in input.split("\n") if line]
+    mymap = {}
+    myll = LinkedList(vals, mymap)
+    for i, val in enumerate(vals):
+        myll.move(mymap[i], val)
+
+    zero_index = vals.index(0)
+    zero = mymap[zero_index]
+    moveby = 1000 % myll.len
+
+    total = 0
+    current = zero
+    for i in range(3):
+        for _ in range(moveby):
+            current = current.next
+        total += current.value
+    yield total
+
+    # Part 2
+    vals = [x * 811589153 for x in vals]
+    mymap = {}
+
+    myll = LinkedList(vals, mymap)
+
+    for _ in range(10):
+        for i, val in enumerate(vals):
+            myll.move(mymap[i], val)
+
+    zero_index = vals.index(0)
+    zero = mymap[zero_index]
+    moveby = 1000 % myll.len
+
+    total = 0
+    current = zero
+    for i in range(3):
+        for _ in range(moveby):
+            current = current.next
+        total += current.value
+
+    # Part 1
+    yield total
